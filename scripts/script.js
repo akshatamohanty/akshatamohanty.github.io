c886fa74871d37795e1dca8862d2c0aa06b2f6a4
Akshata Mohanty <akshatamohanty@gmail.com>
2018-01-31 00:24:33 +0800

    final layout fixed

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	
+	$('#nav-blog').click(function($event){
+		console.log("clicked blog")
+	});
+
+	$('#nav-projects').click(function($event){
+		console.log("clicked projects")
+	});
+
+});
