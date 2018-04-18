9fe3fe81017b195a3a3c76b434bbb54a00ee68da
Travis CI <akshatamohanty+travis@gmail.com>
2018-04-18 22:22:54 +0800

    Lastest site built on successful travis build  auto-pushed to github

---
+++ build.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+set -e
+
+DEPLOY_REPO="https://github.com/akshatamohanty/akshatamohanty.github.io.git"
+
+function main {
+	clean
+	get_current_site
+	build_site
+	deploy
+}
+
+function clean { 
+	echo "cleaning _site folder"
+	if [ -d "_site" ]; then rm -Rf _site; fi 
+}
+
+function get_current_site { 
+	echo "getting latest site"
+	git clone --depth 1 $DEPLOY_REPO _site 
+}
+
+function build_site { 
+	echo "building site"
+	bundle exec jekyll build 
+}
+
+function deploy {
+	echo "deploying changes"
+	cd _site
+	git config --global user.name "Travis CI"
+    git config --global user.email akshatamohanty+travis@gmail.com
+	git add -A
+	git status
+	git commit -m "Lastest site built on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to github"
+	git push $DEPLOY_REPO master:master
+}
+
+main
