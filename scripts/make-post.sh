fecdf228f561ab3dc82d5fbc29473fbf677b1e61
Akshata Mohanty <akshatamohanty@gmail.com>
2020-10-01 13:43:06 +0800

    [Ops] Add script to generate files

---
+++ scripts/make-post.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo Post title?
+read name
+name=${name:="hello-world"}
+echo Choose a category [tech, productivity or books]
+read cate
+cate=${cate:="tech"}
+formattedDate=$(date +"%Y-%m-%d")
+filename=${cate}/_posts/${formattedDate}-${name}.md
+cat > ${filename} <<EOL
+---
+layout: post
+title: ${filename}
+summary:
+date:  ${formattedDate}
+image:
+credits:
+tags:
+ - tag1
+---
+
+### for whom is this?
+
+### why should they care? what is the problem?
+
+### what is the idea/solution?
+
+### headline 1
+
+#### Subpoint 1
+#### Subpoint 2
+
+### headline 2
+
+#### Subpoint 1
+#### Subpoint 2
+
+### headline 3
+
+#### Subpoint 1
+#### Subpoint 2
+
+### takeaway - make it memorable!
+EOL
+
+echo -e "Happy Writing! Your post is here: ${RED}${filename}${NC}"
